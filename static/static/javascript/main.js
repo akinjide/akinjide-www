30115379d006c0431a8004f87d45bf34a8ea461c
Akinjide Bankole <r@akinjide.me>
2018-03-19 12:31:05 +0100

    feat: enable night mode; detection

--- static/static/javascript/main.js
+++ static/static/javascript/main.js
@@ -1,5 +1,33 @@
 $(function() {
   var terminate
+  var hour = (new Date()).getHours()
+  var origin = window.location.origin
+  var $img = $('.index .intro img')
+  var $body = $('body')
+
+
+  /** Page Theme. */
+  if ((hour >= 4) && (hour <= 16)) day()
+  else night()
+
+  function day() {
+    $body.removeClass('dark')
+    $img.attr('src', origin + '/static/images/akinjide-avatar.png')
+  }
+
+  function night() {
+    $body.addClass('dark')
+    $img.attr('src', origin + '/static/images/akinjide-avatar-white.png')
+  }
+
+  $('#toggle').change(function() {
+    if (this.checked) {
+      night()
+      return
+    }
+
+    day()
+  })
 
   /** Article Time. */
   $("main article header h1").on({
